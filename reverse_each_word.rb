24186048f6ce01557bae248ab880c0488d2708fe
Alex Lee <hyunho98@gmail.com>
2019-11-07 23:03:39 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  output = array.collect{|word| word = word.reverse}
-  output.join(" ")
+  array.collect{|word| word = word.reverse}.join(" ")
 end
