587311550b448bcb4b811ef0e37670589c80cc59
Mintra Phophaijit <bluembc@gmail.com>
2019-11-16 00:50:39 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
- def reserve_each_word(sentence1)
-   sentence1.collect do |sentence|
-  puts reservse_string("Hello there, and how are you?")
-  terminal:
-  ["hello", "there", "and", "how","are", "you"]
-  end
+Def reverse_each_word(sentence)
+	Sentence.split.collect {|word| word.reverse}.join(“ “)
+end
