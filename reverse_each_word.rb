089d2054cc5edd587a0a8b3989d51fb53d832cd4
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-25 01:17:41 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ require 'pry'
 def reverse_each_word(sentence1)
   binding.pry
   # "Hello there, and how are you?"
+  sentence1.split('')
  sentence1.each do |f| 
  print f = [] << sentence1.reverse
  end
